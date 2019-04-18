e6aa8e21ddaceb54ae06437e6a91c799917849c5
Aaron Osher <aaron@aaronosher.io>
2019-04-18 21:57:11 +0100

    Add brand icons and add more animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 import Container  from './components/Container/Container';
 import Link from './components/Link/Link';
 import Aaron from './resources/images/Aaron.jpg';
 import './App.scss';
-
 
 class App extends Component {
   render() {
@@ -16,6 +17,18 @@ class App extends Component {
           </div>
           <div className="bio">
             I study computer science at the <Link href="https://cs.nott.ac.uk">University of Nottingham</Link>, am the president of <Link href="https://hacksocnotts.co.uk">HackSoc</Link>, a <Link href="https://githubcampus.expert/aaronosher">GitHub Campus Expert</Link>, and a Coach at <Link href="https://mlh.io/eu">Major League Hacking</Link>.
+          </div>
+
+          <div className="brandButtons">
+            <Link href="https://linkedin.com/in/aaronosher">
+              <FontAwesomeIcon icon={faLinkedinIn} color="black" className="icon" size="2x" />
+            </Link>
+            <Link href="https://twitter.com/aaronosher">
+              <FontAwesomeIcon icon={faTwitter} color="black" className="icon" size="2x" />
+            </Link>
+            <Link href="https://github.com/aaronosher">
+              <FontAwesomeIcon icon={faGithub} color="black" className="icon" size="2x" />
+            </Link>
           </div>
         </div>
       </Container>
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-const Link = (props: {children: Node | string, href: string}) => <a href={props.href} target="_blank" rel="noopener noreferrer">{props.children}</a>;
+const Link = (props: {children: ReactNode , href: string}) => <a href={props.href} target="_blank" rel="noopener noreferrer">{props.children}</a>;
 
 export default Link;
