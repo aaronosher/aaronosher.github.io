11e9fd05e5aa169a59e8f64162fc256ff838a348
Aaron Osher <aaron@aaronosher.io>
2019-04-20 20:50:58 +0100

    Add index.scss to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import AboutPage from './pages/About/About';
+import ProjectsPage from './pages/Projects/Projects';
 
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <Navigation />
         <Switch>
           <Route exact path="/" component={AboutPage} />
+          <Route path="/projects" component={ProjectsPage} />
           <Route component={AboutPage} />
         </Switch>
       </BrowserRouter>
