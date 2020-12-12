3729ba2854a002afc575343d18929bcc4d6abb27
Aaron Osher <git@aaronosher.io>
2020-12-12 16:50:15 +0000

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "../components/Container";
+import { container as containerClass } from "../assets/scss/pages/About.module.scss";
+
+const Page404 = () => {
+  return (
+    <Container>
+      <div className={containerClass}>
+        <h1>
+          <span role="img" aria-label="thinking emoji">
+            🤔
+          </span>
+        </h1>
+        <h2>
+          Something has gone horribly wrong, or this page never existed in the
+          first place.
+        </h2>
+        <p>
+          Next time, try using the navigation bar{" "}
+          <span role="img" aria-label="winking emoji">
+            😉
+          </span>
+        </p>
+      </div>
+    </Container>
+  );
+};
+
+export default Page404;
