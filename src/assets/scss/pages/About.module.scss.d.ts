fffa6887744390b140e0812231bc25fffb33eb55
Aaron Osher <git@aaronosher.io>
2020-12-12 17:35:58 +0000

    Use Gatsby Image for Home Page

--- src/assets/scss/pages/About.module.scss.d.ts
+++ src/assets/scss/pages/About.module.scss.d.ts
@@ -1,4 +1,5 @@
 export const container: string;
+export const portraitContainer: string;
 export const portrait: string;
 export const bio: string;
 export const brandButtons: string;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,58 +11,97 @@ import Greeting from "../components/Greeting";
 import Aaron from "../assets/images/Aaron.jpg";
 import {
   container,
+  portraitContainer,
   portrait,
   bio,
   brandButtons,
 } from "../assets/scss/pages/About.module.scss";
+import { graphql } from "gatsby";
+import Img, { FluidObject } from "gatsby-image";
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <div className={container}>
-          <img src={Aaron} className={portrait} />
-          <Greeting />
-          <div className={bio}>
-            I study computer science at the{" "}
-            <Link href="https://cs.nott.ac.uk">University of Nottingham</Link>,
-            am the president of{" "}
-            <Link href="https://hacksocnotts.co.uk">HackSoc</Link>, a Coach at{" "}
-            <Link href="https://mlh.io/eu">Major League Hacking</Link>, and
-            co-founder of{" "}
-            <Link href="https://hackathons.org.uk">Hackathons UK</Link>.
-          </div>
-
-          <div className={brandButtons}>
-            <Link href="https://linkedin.com/in/aaronosher">
-              <FontAwesomeIcon
-                icon={faLinkedinIn}
-                color="black"
-                className="icon"
-                size="2x"
-              />
-            </Link>
-            <Link href="https://twitter.com/aaronosher">
-              <FontAwesomeIcon
-                icon={faTwitter}
-                color="black"
-                className="icon"
-                size="2x"
-              />
-            </Link>
-            <Link href="https://github.com/aaronosher">
-              <FontAwesomeIcon
-                icon={faGithub}
-                color="black"
-                className="icon"
-                size="2x"
-              />
-            </Link>
-          </div>
-        </div>
-      </Container>
-    );
+interface AppPageProps {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string;
+        description: string;
+        author: string;
+      }
+    }
+    aaron: {
+      childImageSharp: {
+        fluid: FluidObject
+      }
+    }
   }
 }
 
+const App: React.FC<AppPageProps> = ({ data }) => {  
+  return (
+    <Container>
+      <div className={container}>
+        <div className={portraitContainer}>
+          <Img fluid={data.aaron.childImageSharp.fluid} className={portrait} alt='Portrait of Aaron' />
+        </div>
+        <Greeting />
+        <div className={bio}>
+          I study computer science at the{" "}
+          <Link href="https://cs.nott.ac.uk">University of Nottingham</Link>,
+          am the president of{" "}
+          <Link href="https://hacksocnotts.co.uk">HackSoc</Link>, a Coach at{" "}
+          <Link href="https://mlh.io/eu">Major League Hacking</Link>, and
+          co-founder of{" "}
+          <Link href="https://hackathons.org.uk">Hackathons UK</Link>.
+        </div>
+
+        <div className={brandButtons}>
+          <Link href="https://linkedin.com/in/aaronosher">
+            <FontAwesomeIcon
+              icon={faLinkedinIn}
+              color="black"
+              className="icon"
+              size="2x"
+            />
+          </Link>
+          <Link href="https://twitter.com/aaronosher">
+            <FontAwesomeIcon
+              icon={faTwitter}
+              color="black"
+              className="icon"
+              size="2x"
+            />
+          </Link>
+          <Link href="https://github.com/aaronosher">
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="black"
+              className="icon"
+              size="2x"
+            />
+          </Link>
+        </div>
+      </div>
+    </Container>
+  );
+}
+
 export default App;
+
+export const AppPageQuery = graphql`
+  query AppPageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+    aaron: file(name: { eq: "Aaron"}) {
+      childImageSharp {
+        fluid(maxWidth: 540) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
